9df3591d8cc52bd739ecad7c1005bcfb1bc12090
kavya kumari <kavyalohani24@gmail.com>
2026-03-23 23:33:53 +0530

    Fix serializer formatting and improve model field defaults

--- exhibition/models.py
+++ exhibition/models.py
@@ -61,8 +61,8 @@ class ExhibitorInfo(models.Model):
         max_length=190,
         verbose_name=_('Name')
     )
-    linkedin_url = models.URLField(null=True, blank=True)
-    twitter_url = models.URLField(null=True, blank=True)
+    linkedin_url = models.URLField(blank=True,default='')
+    twitter_url = models.URLField(blank=True,default='')
     description = I18nTextField(
         verbose_name=_('Description'),
         null=True,
